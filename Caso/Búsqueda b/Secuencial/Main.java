233ea27acd4aec0c31d748f3569b7ab1708b991a
MariaPinto <38145387+MariaPinto@users.noreply.github.com>
2019-11-11 10:15:01 -0500

    Update Main.java

--- Caso/Búsqueda b/Secuencial/Main.java
+++ Caso/Búsqueda b/Secuencial/Main.java
@@ -1,27 +1,11 @@
-package binary_search;
+package binary_search.secuencial;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
-import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Main extends Thread {
-	
-	int array[];
-	int startindex;
-	int endindex;
-	int key;
-	int result;
-	long time;
-	
-	public Main(int[] array, int startindex, int endindex, int key) {
-		super();
-		this.array = array;
-		this.startindex = startindex;
-		this.endindex = endindex;
-		this.key = key;
-	}
+public class Main {
 
 	int binarySearch(int arr[], int l, int r, int x) 
     { 
@@ -30,7 +14,8 @@ public class Main extends Thread {
   
            
             if (arr[mid] == x) 
-                return mid;
+                return mid; 
+  
             if (arr[mid] > x) 
                 return binarySearch(arr, l, mid - 1, x); 
   
@@ -39,74 +24,35 @@ public class Main extends Thread {
   
         return -1; 
     } 
-	
-	public void run()
-	{
-		long start = System.nanoTime();
-		result = binarySearch(array, startindex, endindex, key);
-		this.time = System.nanoTime() - start;
-	}
   
-	public long getTime()
-	{
-		return this.time;
-	}
-	
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		FileOutputStream os = new FileOutputStream("binary_search_paralelo_java.txt");
+		FileOutputStream os = new FileOutputStream("binary_search_secuencial_java.txt");
 		PrintStream ps = new PrintStream(os);
-		long acumulador = 0;
+		
+		Main ob = new Main(); 
+		
 		int pruebas[] = {1000, 5000, 10000, 15000};
 		
 		for(int i = 0; i < pruebas.length; i++)
 		{
-			int n = pruebas[i]; 
-			int[] arr = new int[n];
-			for(int j = 0; j < n; j++)
+			int[] arr = new int[pruebas[i]];
+			for(int j = 0; j < arr.length; j++)
 			{
 				arr[j] = j;
 			}
 			
 			Arrays.sort(arr);
-			
-	      //  int x = (int) (Math.random()*pruebas[i] + 1);
-	        int x = n - 1;
-			ArrayList<Main> hilos = new ArrayList<Main>();
-
-			
-			for(int k = 0; k < 4; k++)
-			{
-				hilos.add(new Main(arr, k * (n/4), (k + 1 ) * (n / 4), x));
-			}
-			
-			
-			for(int k = 0; k < 4; k++)
-			{
-				hilos.get(k).run();
-			}
-			
-			while(!hilos.isEmpty())
-			{
-				for(int k = 0; k < hilos.size(); k ++)
-				{
-					if(!hilos.get(k).isAlive())
-					{
-						if(hilos.get(k).result != -1)
-						{
-							acumulador += hilos.get(k).getTime();
-						}
-						hilos.remove(k);
-					}
-				}
-			}
-			
-			System.out.println( n + "\t" + (acumulador/1000.00));
-			ps.println( n + "\t" +  (acumulador/1000.00));
+			int n = arr.length; 
+	        int x = n - 1; 
+	        long start = System.nanoTime();
+	        int result = ob.binarySearch(arr, 0, n - 1, x); 
+	        long time = System.nanoTime() - start;
+	        System.out.println(pruebas[i] + "\t" +  (time/1000.00));
+	        ps.println( pruebas[i] + "\t" +  (time/1000.00));
 	        
  		}
         
 	}
 
 }
-
